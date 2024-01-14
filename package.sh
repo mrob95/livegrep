21049c8c69b9d42b23de3b2111e024658c7db43a
Mike Roberts <mike.roberts.2k10@googlemail.com>
2024-01-14 17:28:06 +0000

    fix: support bazelisk in package.sh

--- package.sh
+++ package.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+
+# use bazelisk or bazel, whichever is available
+if ! command -v bazelisk >/dev/null; then
+    if ! command -v bazel >/dev/null; then
+        echo "bazel or bazelisk must be installed"
+        exit 1
+    fi
+    bazel=bazel
+else
+    bazel=bazelisk
+fi
+
 set -ex
 mkdir -p builds
 rev=$(git rev-parse HEAD | head -c10)
 builddir="livegrep-$rev"
 rm -rf "$builddir" && mkdir "$builddir"
-bazel build :livegrep
-tar -C "$builddir" -xf "$(bazel info bazel-bin)"/livegrep.tar
+$bazel build :livegrep
+tar -C "$builddir" -xf "$($bazel info bazel-bin)"/livegrep.tar
 tar -czf "builds/$builddir.tgz" "$builddir"
 rm -rf "$builddir"
 
